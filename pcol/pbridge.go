93779df5fb72569fd7b4f3ff653d63aa3b7ac30b
KtGunn <ktgunn@hotmail.com>
2026-05-15 11:33:21 -0400

    Reslience added to bridge and stream defintions added to all modules. Next step is to trace out the message flow and remove test 'injectors' from the code.

--- pcol/pbridge.go
+++ pcol/pbridge.go
@@ -6,7 +6,11 @@ import (
 	"time"
 	"io"
 	"fmt"	
+
+	"google.golang.org/grpc"
+
 	pb "mockup/proto"
+	 empty "github.com/golang/protobuf/ptypes/empty"
 )
 
 
@@ -45,11 +49,47 @@ func EngageBridge(ctx context.Context) int {
 
 
 func DataStream(ctx context.Context, client pb.BridgeClient) {
-	stream, err := client.Data(ctx)
-	if err != nil {
-		log.Println("No stream ")
-	}
+	for {
+		log.Println("opening head count data stream")
 
+		var stream grpc.ClientStreamingClient[pb.HeadCount, empty.Empty]
+		var closed bool
+		
+		stream, closed = openDataStream(ctx,client)
+		if closed {
+			log.Println("Data Open Context is closed, Bye...")
+			return
+		}
+
+		closed = receiveDataStream(ctx, stream)
+		if closed {
+			log.Println("Data Receive Context is closed, Bye...")
+			return
+		}
+	}
+}
+
+func openDataStream(ctx context.Context,	client pb.BridgeClient) (grpc.ClientStreamingClient[pb.HeadCount, empty.Empty], bool) {
+	
+	for {
+		
+		stream, err := client.Data(ctx)
+		if err == nil {
+			return stream, false
+		}
+		
+		select {
+		case <-ctx.Done():
+			return nil, true
+		case <-time.After(5*time.Second):
+			// we go on
+		}
+	}
+}
+
+func receiveDataStream(ctx context.Context,
+	stream grpc.ClientStreamingClient[pb.HeadCount, empty.Empty]) bool {
+	
 	for {
 		select {
 		case data := <-dataStream:
@@ -65,26 +105,68 @@ func DataStream(ctx context.Context, client pb.BridgeClient) {
 
 
 func StateQueryStream(ctx context.Context, client pb.BridgeClient) {
-	
-	stream, err := client.YourState(ctx)
-	if err != nil {
-		log.Println("No stream ")
-	}
+	for {
+		log.Println("Opening the data stream...")
 
+		
+		var stream grpc.BidiStreamingClient[pb.StateResponse,pb.StateQuery]
+		var closed bool
+
+		stream, closed = openStateStream(ctx, client)
+		if closed {
+			log.Println(" Data context closed ")
+			return
+		}
+
+		closed = doStateStream(ctx, stream)
+		if closed {
+			log.Println(" Receive context closed ")
+			return
+		}
+	}
+}
+
+
+// openStateStream
+//
+func openStateStream(ctx context.Context,
+	client pb.BridgeClient) (grpc.BidiStreamingClient[pb.StateResponse,pb.StateQuery], bool) {
+	
+	for {
+		
+		stream, err := client.YourState(ctx)
+		if err == nil {
+			return stream, false
+		}
+		
+		select {
+		case <-ctx.Done():
+			return nil, true
+		case <-time.After(5*time.Second):
+			// we go on
+		}
+	}
+}
+
+func doStateStream(ctx context.Context,
+	stream grpc.BidiStreamingClient[pb.StateResponse,pb.StateQuery]) bool {
+	
 	go func() {
 		for {
 			query, err := stream.Recv()
 			
 			if err == io.EOF {
 				log.Println("BS eof", err)
+				return
 			}
 			if err != nil {
 				log.Println("state response stream error", err)
+				return
 			}
 			
 			log.Println("pBb recv:" , query)
 
-			stateResponse<-"Here's your reort!"
+			stateResponse<-"Here's your retort!"
 		}
 	}()
 	
@@ -98,6 +180,9 @@ func StateQueryStream(ctx context.Context, client pb.BridgeClient) {
 			}
 			stream.Send(outmsg)
 			
+		case <-ctx.Done():
+			return true
+
 		case <-time.After(12*time.Second):
 			log.Println("..pbridge tick")
 		}
